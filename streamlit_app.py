f558697a55b578f165a93408aeb8b95c3000aada
learning-789 <124663223+learning-789@users.noreply.github.com>
2023-02-07 13:31:08 +0530

    Update streamlit_app.py
    adding header 
    adding text to break fast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title( 'My Parents New heaklthy Diner')
+
+streamlit.header( 'breakfast Menu')
+streamlit.text( 'Omega 3 & blueberry Oatmeal')
+streamlit.text( 'Kale ,Spanish & Rocket Smoothie')
+streamlit.text( 'Hard-boiled free - Range Egg')
